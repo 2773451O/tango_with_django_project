234a1881a82d123b4c9fabe21f6903cb900450f2
“James <2773451O@student.gla.ac.uk>
2024-02-02 15:24:45 +0000

    Chapter 7 commit

--- populate_rango.py
+++ populate_rango.py
@@ -5,9 +5,7 @@ import django
 django.setup()
 from rango.models import Category, Page
 def populate():
-    # First, we will create lists of dictionarier'<a(\s+)href="/rango/">(\w+)</a>'s containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories. # This might seem a little bit confusing, but it allows us to iterate # through each data structure, and add the data to our models.
+   
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/',
--- rango/models.py
+++ rango/models.py
@@ -1,33 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-
-# Create your models here.
 class Category(models.Model):
+    NAME_MAX_LENGTH = 128
 
-    name = models.CharField(max_length=128, unique=True) 
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
-    
+
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name) 
+        self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = 'Categories'
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
 
-
-from django.template.defaultfilters import slugify
-
 class Page(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
-    views = models.IntegerField(default=0)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
 
-    def __str__(self): 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
         return self.title
     
